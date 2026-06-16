c74898d17498f44571658ae04e017f378b489061
felixdahm <felix.dahm@itsd-consulting.de>
2026-06-16 21:34:12 +0200

    UST ID raus

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.sebauservice.de',
+  site: 'https://sebauservice.de',
   integrations: [sitemap()],
 });
